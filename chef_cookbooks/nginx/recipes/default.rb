b4b2c6cacffbffce9dd5ee5158694b869856214d
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2018-02-13 10:07:28 +0100

    changed to setup the default.conf for nginx

--- chef_cookbooks/nginx/recipes/default.rb
+++ chef_cookbooks/nginx/recipes/default.rb
@@ -3,24 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-
-
-
-
 package 'nginx' do
   action :install
 end
-
 
 # Remove existing directory
 if Dir.exists?(node['nginx']['default']['document_root'])
     Chef::Log.info "Directory exitst, will remove '#{node['nginx']['default']['document_root']}' from node"
     directory node['nginx']['default']['document_root'] do
       action :delete
+      recursive true
     end
 end
 
-# Create eh default document root
+# Create the default document root
 directory node['nginx']['default']['document_root'] do
   owner 'vagrant'
   group 'vagrant'
@@ -29,10 +25,40 @@ directory node['nginx']['default']['document_root'] do
   action :create
 end
 
-# create the nginx default 
+# create the nginx default index
 template "#{node['nginx']['default']['document_root']}/index.html" do
   source 'index.html.erb'
   owner 'vagrant'
   group 'vagrant'
   mode '0755'
+end
+
+# check is nginx default symlink is set ?
+nginxDefaultConfig = '/etc/nginx/sites-available/default'
+if File.exists?(nginxDefaultConfig)
+  file nginxDefaultConfig do
+    action :delete
+  end
+end
+
+# Add the config template
+template "/etc/nginx/sites-available/default.conf" do
+  source 'default.conf.erb'
+end
+
+# check is nginx default symlink is set ?
+nginxDefaultConfigSymlink = '/etc/nginx/sites-enabled/default'
+link nginxDefaultConfigSymlink do
+  action :delete
+  only_if "test -L #{nginxDefaultConfigSymlink}"
+end
+
+link nginxDefaultConfigSymlink do
+  to "/etc/nginx/sites-available/default.conf"
+  link_type :symbolic
+end
+
+# restart the nginx
+service 'nginx' do
+  action :restart
 end
