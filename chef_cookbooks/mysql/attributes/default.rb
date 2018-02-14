6a8209541703eb46df614acace53f98d96173c7a
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2018-02-14 17:52:25 +0100

    check for debian > 9 version ( Debian9 uses the mariadb instead mysql )

--- chef_cookbooks/mysql/attributes/default.rb
+++ chef_cookbooks/mysql/attributes/default.rb
@@ -1,5 +1,14 @@
 default['mysql']['default']['sqldump_directory'] = 'files/sql_dumps/'
 default['mysql']['default']['sqldump_copy_directory'] = '/tmp/sql_dumps/'
+default['mysql']['default']['bind-address'] = '0.0.0.0'
+
+
+if node['platform'] == 'debian' && Gem::Version.new(node['platform_version']) >= Gem::Version.new(9)
+  default['mysql']['default']['config_dir'] = '/etc/mysql/mariadb.conf.d'
+else
+  default['mysql']['default']['config_dir'] = '/etc/mysql/mysql.conf.d'
+end
+
 
 default['mysql']['default']['users'] = {
   "root" => {
