a522474ea9582b2958c94f52ee882129be5291a3
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2018-02-14 11:00:56 +0100

    changed to iterate of the defined folder to restore the DB

--- chef_cookbooks/mysql/recipes/schema-restore.rb
+++ chef_cookbooks/mysql/recipes/schema-restore.rb
@@ -4,27 +4,38 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-
 # Create tmp sql dump directory
-directory '/tmp/sql_dumps' do
+directory "#{node['mysql']['default']['sqldump_copy_directory']}" do
   mode '0755'
   recursive true
   action :create
 end
 
-# Copy the sql dumps
-cookbook_file "copy sql dumps" do
-  source 'sql_dumps/test_db.sql'
-  path '/tmp/sql_dumps/test_db.sql'
-  mode '0755'
-  action :create
+
+Dir.glob("#{__dir__}/../#{node['mysql']['default']['sqldump_directory']}/*.sql").each do |sqlDump|
+
+  destFileName = "#{node['mysql']['default']['sqldump_copy_directory']}#{File.basename(sqlDump)}"
+
+  # Copy the sql dumps
+  file "copy sql dumps '#{sqlDump}'" do
+    mode '0755'
+    action :create
+    path destFileName
+    content IO.read(sqlDump)
+  end
+
+  # import dump
+  script "Import Dump from #{destFileName}" do
+    interpreter 'bash'
+    user 'root'
+    code <<-EOF
+      sudo mysql --user=root --comments < #{destFileName}
+    EOF
+  end
 end
 
-script "Import Dump" do
-  interpreter 'bash'
-  user 'root'
-  #cwd '/tmp'
-  code <<-EOF
-    sudo mysql --user=root --comments < /tmp/sql_dumps/test_db.sql
-  EOF
+# Remove the dump directory
+directory "#{node['mysql']['default']['sqldump_copy_directory']}" do
+  recursive true
+  action :delete
 end
