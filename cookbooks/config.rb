7f26894fe013fcb19ec9f88431fdb07063d538a8
Markus <markus@simpleasthat.de>
2018-02-15 10:56:09 +0100

    global vagrant config added

---
+++ cookbooks/config.rb
@@ -0,0 +1,18 @@
+# Global Config File
+
+$VAGRANTFILE_API_VERSION        = "2"
+$VAGRANTFILE_BOX                = "ubuntu/xenial64"
+$VAGRANTFILE_HOST_IP            = "192.168.33.1"
+$VAGRANTFILE_NETWORK_IP         = "192.168.33.10"
+$VAGRANTFILE_NETWORK_GUEST_PORT = 80
+$VAGRANTFILE_NETWORK_HOST_PORT  = 8080
+$VAGRANTFILE_SSH_AGENT_FORWARD  = true
+
+$VM_NAME        = "ubuntu_xenial64"
+$VM_SHOW_GUI    = false
+$VM_MEMORY 	    = 2048
+$VM_CPUS		= 2
+
+$VM_SYNC_FOLDERS = [
+    { :HOST_PATH => "/var/www", :GUEST_PATH => "/vagrant_data", :TYPE => "nfs" },
+]
