c0e1c3864d7cb252769cd881c65223f8b5a62b0a
Markus <msommerfeld@silversurfer7.de>
2018-04-10 17:00:05 +0200

    project root folder added

--- cookbooks/_chef_resources/nginx/attributes/default.rb
+++ cookbooks/_chef_resources/nginx/attributes/default.rb
@@ -1,6 +1,7 @@
 majorVersionNumber, *rest = node['platform_version'].split(/\./)
 
-default['nginx']['default']['document_root'] =              '/vagrant_data'
+default['nginx']['default']['document_root'] =              '/var/www/html'
+default['nginx']['default']['project_root'] =               '/vagrant_data'
 default['nginx']['default']['document_root_autoindex'] =    'on'
 default['nginx']['default']['config_path'] =                '/etc/nginx/'
 default['nginx']['default']['server_suffix'] =              "#{node['platform']}#{majorVersionNumber}"
