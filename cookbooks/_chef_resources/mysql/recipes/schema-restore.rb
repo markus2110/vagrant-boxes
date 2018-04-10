cad691f18c510c9f9a433db7f53eece145a599a5
Markus <msommerfeld@silversurfer7.de>
2018-04-10 15:00:49 +0200

    changed to copy SQL Files from vagrant sql_dumps folder

--- cookbooks/_chef_resources/mysql/recipes/schema-restore.rb
+++ cookbooks/_chef_resources/mysql/recipes/schema-restore.rb
@@ -4,6 +4,7 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+# Copy all SQL DUMPs from mysql->fiels->sql_dumps
 # Create tmp sql dump directory
 directory "#{node['mysql']['default']['sqldump_copy_directory']}" do
   mode '0755'
@@ -11,11 +12,9 @@ directory "#{node['mysql']['default']['sqldump_copy_directory']}" do
   action :create
 end
 
-
+# Copy all SQL files from mysql recipe
 Dir.glob("#{__dir__}/../#{node['mysql']['default']['sqldump_directory']}/*.sql").each do |sqlDump|
-
   destFileName = "#{node['mysql']['default']['sqldump_copy_directory']}#{File.basename(sqlDump)}"
-
   # Copy the sql dumps
   file "copy sql dumps '#{sqlDump}'" do
     mode '0755'
@@ -23,16 +22,31 @@ Dir.glob("#{__dir__}/../#{node['mysql']['default']['sqldump_directory']}/*.sql")
     path destFileName
     content IO.read(sqlDump)
   end
+end
 
-  # import dump
-  script "Import Dump from #{destFileName}" do
+# Copy all SQL files from VAGRANT sync folder
+Dir.glob("/vagrant/sql_dumps/*.sql").each do |sqlDump|
+  destFileName = "#{node['mysql']['default']['sqldump_copy_directory']}#{File.basename(sqlDump)}"
+  # Copy the sql dumps
+  file "copy sql dumps '#{sqlDump}'" do
+    mode '0755'
+    action :create
+    path destFileName
+    content IO.read(sqlDump)
+  end
+end
+
+# Restore Schema
+Dir.glob("#{node['mysql']['default']['sqldump_copy_directory']}/*.sql").each do |sqlDump|
+  script "Import Dump from #{sqlDump}" do
     interpreter 'bash'
     user 'root'
     code <<-EOF
-      sudo mysql --user=root --comments < #{destFileName}
+      sudo mysql --user=root --comments < #{sqlDump}
     EOF
   end
 end
+
 
 # Remove the dump directory
 directory "#{node['mysql']['default']['sqldump_copy_directory']}" do
