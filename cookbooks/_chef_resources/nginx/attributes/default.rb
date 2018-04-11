6869736f23175649ae69ee35c183c5ebcf0ce6d6
Markus <msommerfeld@silversurfer7.de>
2018-04-11 12:07:09 +0200

    nginx ssl config dir added

--- cookbooks/_chef_resources/nginx/attributes/default.rb
+++ cookbooks/_chef_resources/nginx/attributes/default.rb
@@ -4,4 +4,5 @@ default['nginx']['default']['document_root'] =              '/var/www/html'
 default['nginx']['default']['project_root'] =               '/vagrant_data'
 default['nginx']['default']['document_root_autoindex'] =    'on'
 default['nginx']['default']['config_path'] =                '/etc/nginx/'
+default['nginx']['default']['ssl_path'] =                   '/etc/nginx/conf.d/ssl/'
 default['nginx']['default']['server_suffix'] =              "#{node['platform']}#{majorVersionNumber}"
