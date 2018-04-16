b070a41a8160341f5d82f4492e37fefdc3209ae8
Markus <msommerfeld@silversurfer7.de>
2018-04-16 17:09:55 +0200

    nodejs not supported on debian 7

--- cookbooks/_chef_resources/php/recipes/nodejs.rb
+++ cookbooks/_chef_resources/php/recipes/nodejs.rb
@@ -5,12 +5,14 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 # Install Nodejs
-script "Install NodeJs" do
-  interpreter 'bash'
-  user 'root'
-  code <<-EOF
-    curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-    sudo npm install -g grunt gulp
-  EOF
+if (node['platform'] == 'debian' && Gem::Version.new(node['platform_version']) >= Gem::Version.new(8)) || (node['platform'] == 'ubuntu' ) 
+  script "Install NodeJs #{node['platform_version']}" do
+    interpreter 'bash'
+    user 'root'
+    code <<-EOF
+      curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+      sudo apt-get install -y nodejs
+      sudo npm install -g grunt gulp
+    EOF
+  end
 end
