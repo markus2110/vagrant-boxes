e8d9c960a0c60d424baf2be223689eecc82dc13c
Markus <msommerfeld@silversurfer7.de>
2018-11-15 23:42:38 +0100

    nginx recipe added

--- cookbooks/nginx_php/metadata.rb
+++ cookbooks/nginx_php/metadata.rb
@@ -7,14 +7,7 @@ long_description 'Installs/Configures nginx_php'
 version '0.1.0'
 chef_version '>= 13.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/nginx_php/issues'
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/nginx_php'
+
+depends 'environment_essentials'
+depends 'nginx'
--- cookbooks/nginx_php/recipes/default.rb
+++ cookbooks/nginx_php/recipes/default.rb
@@ -3,3 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+include_recipe 'environment_essentials::default'
+include_recipe 'nginx::default'
