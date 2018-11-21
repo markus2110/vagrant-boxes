e7f336de931197bb322ca220c9fe343d30e6b92a
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2018-11-21 18:58:26 +0100

    changed to use chef supermarket recipes

--- cookbooks/apache_php/metadata.rb
+++ cookbooks/apache_php/metadata.rb
@@ -1,20 +1,6 @@
 name 'apache_php'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures apache_php'
-long_description 'Installs/Configures apache_php'
 version '0.1.0'
 chef_version '>= 13.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/apache_php/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/apache_php'
+depends 'mysql'
+depends 'apache2'
--- cookbooks/apache_php/recipes/default.rb
+++ cookbooks/apache_php/recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+include_recipe 'apache2::default'
+
+apache_conf 'default-site' do
+  enable true
+end
+
+mysql_service '5.7' do
+  port '3306'
+  version '5.7'
+  initial_root_password 'change me'
+  action [:create, :start]
+end
