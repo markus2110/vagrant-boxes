b1f5bca0ee8aeca8c38c22296067c6669a09c3f0
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2018-11-22 09:06:20 +0100

    mysql added

--- site-cookbooks/apache_php/recipes/mysql.rb
+++ site-cookbooks/apache_php/recipes/mysql.rb
@@ -4,9 +4,24 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-mysql_service '5.7' do
+mysql_instance = node['mysql']['instance_name']
+
+mysql_service "#{mysql_instance}" do
   port '3306'
   version '5.7'
-  initial_root_password 'change me'
+  initial_root_password 'password'
   action [:create, :start]
+end
+
+
+mysql_config "#{mysql_instance} - Bind-Address Config" do
+  source 'mysql/mysqld_bind_address.cnf.erb'
+  notifies :restart, "mysql_service[#{mysql_instance}]"
+  action :create
+end
+
+mysql_config "#{mysql_instance} - SQL-Mode Config" do
+  source 'mysql/mysqld_sql_mode.cnf.erb'
+  notifies :restart, "mysql_service[#{mysql_instance}]"
+  action :create
 end
